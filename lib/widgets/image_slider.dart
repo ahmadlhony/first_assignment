4b88ebd122407dcfee0a485adc0162d2a674ed37
ahmadlhony <ahmadlhony@outlook.com>
2021-10-19 19:46:53 +0300

    dot indicator

--- lib/widgets/image_slider.dart
+++ lib/widgets/image_slider.dart
@@ -11,22 +11,70 @@ class ImageSlider extends StatelessWidget {
       height: 240,
       child: AspectRatio(
         aspectRatio: 16 / 9,
-        child: CarouselSlider(
-          items: [
-            Image.asset(
-              'assets/images/first.jpg',
-              fit: BoxFit.cover,
+        child: Stack(children: [
+          CarouselSlider(
+            items: [
+              Image.asset(
+                'assets/images/first.jpg',
+                fit: BoxFit.cover,
+              ),
+              Image.asset(
+                'assets/images/second.jpg',
+                fit: BoxFit.cover,
+              ),
+              Image.asset(
+                'assets/images/third.jpg',
+                fit: BoxFit.cover,
+              ),
+            ],
+            options: CarouselOptions(
+              viewportFraction: 1,
+              height: 400,
+              aspectRatio: 16 / 9,
+              enableInfiniteScroll: true,
             ),
-            Image.asset('assets/images/second.jpg'),
-            Image.asset('assets/images/third.jpg'),
-          ],
-          options: CarouselOptions(
-            viewportFraction: 1,
-            height: 400,
-            aspectRatio: 16 / 9,
-            enableInfiniteScroll: true,
           ),
-        ),
+          Positioned(
+            bottom: 5,
+            child: Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 20,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.black26,
+                      radius: 5,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 4,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(3)),
+                    CircleAvatar(
+                      backgroundColor: Colors.black,
+                      radius: 5,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 4,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(3)),
+                    CircleAvatar(
+                      backgroundColor: Colors.black,
+                      radius: 5,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 4,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ]),
       ),
     );
   }
